d3576681a36f45b7cc4b176296e8998006b82b00
badjfas <bjwkor@naver.com>
2020-12-07 13:48:04 +0900

    First steps with Typescript

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "bjw", age = 24, gender = "male";
+const HelloFn = (name, age, gender) => {
+    console.log(name, age, gender);
+};
+HelloFn(name, age, gender);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("hello")
+const name="bjw",
+    age=24,
+    gender="male"
+  
+  const HelloFn = (name:string,age,gender)=>{
+        console.log(name,age,gender)
+    }
+    HelloFn(name,age,gender)
+    export {}
